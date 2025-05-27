2d422e77427d3a95dee1a6fddbed83fb3c4ac531
DCajiao <dcajiaolazt@gmail.com>
2025-05-27 17:55:29 -0500

    ✨ Feat: add definitions for project variables and constants including paths and API endpoints

---
+++ app/src/utils/definitions.py
@@ -0,0 +1,8 @@
+# This file contains definitions for variables and constants used across the project.
+
+# PATHS
+TRAINED_MODEL_PATH = "model/"
+TRAINING_DATA_PATH = "data/training_set"
+
+# API ENDPOINTS
+URL_API_SQL_VALIDATOR = "https://www.eversql.com/api/checkSqlSyntax"
